c3b948f8b62418b3adf748e342c340270ca3f310
Jamie Laing <jamie@jamielaing.com>
2016-05-19 09:07:38 -0500

    Add project files.

---
+++ JML_SensorLibrary/JML_SensorLibrary.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		JML_SensorLibrary.ino
+ Created:	5/19/2016 9:07:36 AM
+ Author:	jlaing
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "JML_SensorLibraryLib.h"
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ JML_SensorLibrary/src/_micro-api/libraries/JML_SensorLibraryLib/src/JML_SensorLibraryLib.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		JML_SensorLibraryLib.cpp
+ Created:	5/19/2016 9:07:36 AM
+ Author:	jlaing
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "JML_SensorLibraryLib.h"
+
+
